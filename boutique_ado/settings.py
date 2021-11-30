8dad2a894507c40f0b71bee570751515f158a3fa
deborasantos28 <debora_hardy@hotmail.com>
2021-11-30 21:54:25 +0000

    Added basicheckout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,9 +42,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    
-    # Other
 
+    # Other
     'crispy_forms',
 ]
 
@@ -60,9 +59,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 
 TEMPLATES = [
     {
@@ -167,5 +164,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
